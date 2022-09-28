c14dbe769cd66766760e22bbaeb38eef6eff6e79
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-28 19:14:19 -0300

    Custom cursor

--- gallery-custom-cursor.js
+++ gallery-custom-cursor.js
@@ -7,7 +7,7 @@ gsap.set(".cursor_text", {opacity: 0, y: "4rem", duration: 0.3});
 $('.is-flip-to').mouseenter(function() {
     $('body').css('cursor', 'none');
     $('.cursor_dot').css( {"height": cursorGrowSize, "width": cursorGrowSize});
-    $('.cursor_text').text($(this).siblings('.tour-tooltip').text());
+    $('.cursor_text').text($(this).siblings('.gallery-tooltip').text());
     $('.cursor_text').css("opacity", "1");
     gsap.to(".cursor_text", {opacity: 1, y: "0rem", duration: 0.3});
 });
