b2fd5cf6ca84c8ea709258a1698ac7b1e8ce3f1d
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-28 19:07:22 -0300

    School Gallery Loader

---
+++ school-gallery-loader.js
@@ -0,0 +1,24 @@
+let loader = gsap.timeline();
+loader
+.set(".gallery-video_item", { boxShadow: "none" })
+.from(".gallery_loader-img", {
+    scaleX: 1.1,
+    scaleY: 1.1,
+    opacity: 0,
+    duration: 0.5,
+    stagger: { each: 0.2 },
+    onComplete: () => {
+        let state = Flip.getState(".gallery_loader-img");
+        Flip.from(state, {
+            targets: ".is-flip-to",
+            duration: 0.8,
+            stagger: { each: 0.2, from: "end" },
+            ease: "power1.inOut",
+            onComplete: () => {
+                $(".gallery-video_item").css("box-shadow", "0 0 10px 2px hsla(0, 0.00%, 100.00%, 0.50)")
+            }
+        });
+    }
+})
+.to(".gallery_loader", { opacity: 0 })
+.set(".gallery_loader", { display: "none" })
