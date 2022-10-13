b0ad7d063c3f18403b8f343a1e8a097594f050d3
Alberto de Olivira <betodeoliveira@outlook.com>
2022-10-13 15:39:13 -0300

    Updated gallery hover trigger

--- gallery-custom-cursor.js
+++ gallery-custom-cursor.js
@@ -4,7 +4,7 @@ let cursorGrowSize = "10rem";
 $('.cursor_dot').css( {"height": cursorSize, "width": cursorSize});
 gsap.set(".cursor_text", {opacity: 0, y: "4rem", duration: 0.3});
 
-$('.is-flip-to').mouseenter(function() {
+$('.gallery-video_item, .gallery-gallery_item').mouseenter(function() {
     $('body').css('cursor', 'none');
     $('.cursor_dot').css( {"height": cursorGrowSize, "width": cursorGrowSize});
     $('.cursor_text').text($(this).siblings('.gallery-tooltip').text());
@@ -12,7 +12,7 @@ $('.is-flip-to').mouseenter(function() {
     gsap.to(".cursor_text", {opacity: 1, y: "0rem", duration: 0.3});
 });
 
-$('.is-flip-to').mouseleave(function() {
+$('.gallery-video_item, .gallery-gallery_item').mouseleave(function() {
     $('body').css('cursor', 'auto');
     $('.cursor_dot').css( {"height": cursorSize, "width": cursorSize});
     gsap.killTweensOf(".cursor_text");
