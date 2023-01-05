1ca6a2069408c7ba89020535a37c7daa74327f57
Alberto de Olivira <betodeoliveira@outlook.com>
2023-01-05 12:01:00 -0300

    Updating logo on gallery

--- background-color-changer.js
+++ background-color-changer.js
@@ -14,7 +14,7 @@ $(".background-trigger").each(function (index) {
                 $("body").css("color", "#f8f8f8");
                 $(".navbar_brand-divider").css("background-color", "#ffffff");
                 $(".navbar_links-bg").css("background-color", "#26344f");
-                $(".navbar_brand-asfam").css({"-webkit-filter" : "brightness(10)"});
+                $(".navbar_brand-asfam").css({"-webkit-filter" : "brightness(100)"});
                 $(".navbar_brand-eduqua").attr("src", "https://uploads-ssl.webflow.com/6323f27645f8607e87793e8f/63325fbbbac9a46ca8561c16_eduqua-logo-white.png");
             }
         },
